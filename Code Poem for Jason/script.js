d117f202c0ca340ee2d0fc79e527ac854b7090b6
jonnyblco <jonnyblco@gmail.com>
2019-07-03 09:25:01 -0400

    Add files via upload

---
+++ Code Poem for Jason/script.js
@@ -0,0 +1,14 @@
+var name = prompt ("What is your name?");
+alert("Hello, " + name);
+var tea = prompt ("What hot drink do you brew with a bag?")
+var roll = prompt ("What is the fun way to go down a hill?")
+var day = prompt ("What is the opposite of night?")
+
+alert("A Poem for My Love");
+
+alert("Dear " + name);
+alert("You are my sweet " + tea);
+alert("You are my cinnamon " +roll);
+alert("I love you " + name);
+alert("Thanks for being nice to me! Have a nice " + day);
+alert("!!!");
